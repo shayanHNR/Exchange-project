8cd0897f436cb62e6e8494a6f41277076876ae2f
ShayanHenareh <namelesskurd7@gmail.com>
2026-07-13 19:25:18 +0330

    add currency model

--- exchange/models.py
+++ exchange/models.py
@@ -8,3 +8,10 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Currency(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
