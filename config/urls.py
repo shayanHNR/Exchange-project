a9be51f379dc9c28985e082ea7a464c5eacbdcd7
ShayanHenareh <namelesskurd7@gmail.com>
2026-07-15 11:59:42 +0330

    add customer report

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from exchange import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('customer/<int:customer_id>/report/', views.customer_report),
 ]
 urlpatterns += static(
     settings.MEDIA_URL,
--- exchange/views.py
+++ exchange/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Customer
+
+
+def customer_report(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+
+    balance = customer.get_balance()
+    transactions = customer.transaction_set.all().order_by('-created_at')
+
+    return render(
+        request,
+        'exchange/customer_report.html',
+        {
+            'customer': customer,
+            'balance': balance,
+            'transactions': transactions,
+        }
+    )
